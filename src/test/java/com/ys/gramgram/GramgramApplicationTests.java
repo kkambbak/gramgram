6d30f0d142b5fbcb0c0621262a9d695b94cda26a
kkambbak <yunsung9841@gmail.com>
2023-03-29 15:58:15 +0900

    setting

---
+++ src/test/java/com/ys/gramgram/GramgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.gramgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
